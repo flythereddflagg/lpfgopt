829cf6b85f6d4b56158107f243c7b3288e24211c
Mark Redd <redddogjr@gmail.com>
2019-04-22 06:39:11 -0600

    added more tests and updated .gitignore

--- tests/benchmark_tests.py
+++ tests/benchmark_tests.py
@@ -2,7 +2,11 @@ import numpy as np
 from lpfgopt import minimize
 
 
-def run(f, bounds, check, options={}, output=False, tol=1e-1):
+def run(f, bounds, check, options={}, output=False, tol=1e-3):
+    '''
+    Runs a benchmark test with the given parameters and causes a
+    failing result for nosetests on a failure.
+    '''
     sol = minimize(f, bounds, **options)
     
     for key, value in sol.items():
@@ -20,7 +24,7 @@ def run(f, bounds, check, options={}, output=False, tol=1e-1):
         else:
             norm = check[i]
         err = abs((check[i] - sol['x'][i])/norm)
-        assert err <= tol, f"Failed on parameter index {i}"
+        assert err <= tol, f"Failed on parameter index {i} with error {err}"
     
     if output:
         raise Exception("Generic Exception")
@@ -36,6 +40,7 @@ def rastrigin_test():
         "tol"         : 1e-3,
         "seedval"     : 4815162342,
         }
+        
     A = 10
     f = lambda x: 10*len(x) + sum([i**2 - A*np.cos(2*np.pi*i) for i in x])
     
@@ -56,6 +61,7 @@ def ackley_test():
         "tol"         : 1e-3,
         "seedval"     : 4815162342,
         }
+        
     f = lambda x: -20*np.exp(-0.2 * np.sqrt(0.5*(x[0]**2 + x[1]**2))) -\
                   np.exp(0.5*(np.cos(2*np.pi*x[0]) + np.cos(2*np.pi*x[1]))) +\
                   np.e + 20
@@ -78,6 +84,7 @@ def sphere_test():
         "tol"         : 1e-3,
         "seedval"     : 4815162342,
         }
+        
     f = lambda x: sum([i**2 for i in x])
     
     bounds = [
@@ -97,6 +104,7 @@ def rosenbrock_test():
         "tol"         : 1e-3,
         "seedval"     : 4815162342,
         }
+        
     f = lambda x: (1 - x[0])**2 + 100*(x[1] - x[0]**2)**2
     
     bounds = [
@@ -106,5 +114,52 @@ def rosenbrock_test():
     check = [1.0, 1.0]
     
     run(f, bounds, check, options)
+
+
+
+def beale_test():
+    """
+    Beale function benchmark
+    """
+    options = {
+        "tol"         : 1e-3,
+        "seedval"     : 4815162342,
+        }
         
+    f = lambda x:  (1.5 - x[0] + x[0]*x[1])**2\
+                 + (2.25 - x[0] + x[0]*x[1]**2)**2\
+                 + (2.625 - x[0] + x[0]*x[1]**3)**2
+    
+    bounds = [
+        [-4.5, 4.5],
+        [-4.5, 4.5]]
+    
+    check = [3.0, 0.5]
+    
+    run(f, bounds, check, options)
+
+
+
+def goldstein_price_test():
+    """
+    Goldstein–Price function benchmark
+    """
+    options = {
+        "tol"         : 1e-3,
+        "seedval"     : 4815162342,
+        }
         
+    f = lambda x:   (1 + (x[0] + x[1] + 1)**2\
+                  * (19 - 14*x[0] + 3*x[0]**2 - 14*x[1] + 6*x[0]*x[1]\
+                  + 3*x[1]**2))\
+                  * (30 + (2*x[0] - 3*x[1])**2\
+                  * (18 - 32*x[0] + 12*x[0]**2 + 48*x[1] - 36*x[0]*x[1]\
+                  + 27*x[1]**2))
+    
+    bounds = [
+        [-2.0, 2.0],
+        [-2.0, 2.0]]
+    
+    check = [0.0, -1.0]
+    
+    run(f, bounds, check, options)
