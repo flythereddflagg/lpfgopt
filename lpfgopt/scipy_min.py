029b41bc1515254056eb1dbf3a7ab273a4de0a75
Mark <redddogjr@gmail.com>
2019-10-04 14:49:28 -0600

    added scipy test for c

--- lpfgopt/scipy_min.py
+++ lpfgopt/scipy_min.py
@@ -1,5 +1,7 @@
 from lpfgopt.leapfrog import LeapFrog
+from lpfgopt.c_leapfrog import minimize as c_minimize
 from scipy.optimize import OptimizeResult
+
 
 def leapfrog_method(fun, x0, args, **kwargs):
     """
@@ -7,40 +9,44 @@ def leapfrog_method(fun, x0, args, **kwargs):
     basic usage:
         from lpfgopt.scipy_min import leapfrog_method
         from scipy.optimize import minimize
-        
-        solution = minimize(fun, [0], method=leapfrog_method, 
+
+        solution = minimize(fun, [0], method=leapfrog_method,
                             bounds=bounds, options=options)
         print(solution.x)
-        
+
     Other options can be passed into the dictionary 'options' argument. x0 has
     no meaning in this context so it is just filled with an arbitrary object.
-    
+
     NOTES:
         - the 'constraints' variable is ignored for the leapfrog_method. To pass
-            in constraints, a constraint function may be passed in as 
-            the 'fconstraint' option having the form: fconstraint(x) <= 0. 
-            The function should return a value greater than 0 whenever any 
+            in constraints, a constraint function may be passed in as
+            the 'fconstraint' option having the form: fconstraint(x) <= 0.
+            The function should return a value greater than 0 whenever any
             constraint is violated. The optimizer checks this function for every
-            new point and punishes objective when it is violated. 'fconstraint' 
-            is passed in the options dictionary (see lpfgopt.leapfrog.LeapFrog 
+            new point and punishes objective when it is violated. 'fconstraint'
+            is passed in the options dictionary (see lpfgopt.leapfrog.LeapFrog
             for more details)
     """
     kwargs["fun"]  = fun
     kwargs["x0"]   = x0
     kwargs["args"] = args
-    
-    lf = LeapFrog(**kwargs)
-    solution = lf.minimize()
-    
+
+    use_clib = False if "use_clib" not in kwargs else kwargs["use_clib"]
+    if use_clib:
+        solution = c_minimize(**kwargs)
+    else:
+        lf = LeapFrog(**kwargs)
+        solution = lf.minimize()
+
     opt_res = OptimizeResult()
-    
+
     for key, val in solution.items():
         opt_res[key] = val
-        
+
     opt_res["jac"]      = None
     opt_res["hess"]     = None
     opt_res["hess_inv"] = None
     opt_res["njev"]     = 0
     opt_res["nhev"]     = 0
-    
+
     return opt_res
--- tests/test_numpy_scipy.py
+++ tests/test_numpy_scipy.py
@@ -14,7 +14,7 @@ _intvls = np.array([
     [-10.0, 10.0]])
 
 options = {
-    "fun"         : _f, 
+    "fun"         : _f,
     "bounds"      : _intvls,
     "args"        : (),
     "points"      : 10,
@@ -26,40 +26,81 @@ options = {
     "pointset"    : None
     }
 
+
+options_c = {
+    "fun"         : _f,
+    "bounds"      : _intvls,
+    "args"        : (),
+    "points"      : 10,
+    "fconstraint" : _g1,
+    "discrete"    : np.array([0,1]),
+    "maxit"       : 5000,
+    "tol"         : 1e-3,
+    "seedval"     : 1235,
+    "pointset"    : None,
+    "use_clib"    : True
+    }
+
+
 def test_numpy_test():
     """
     numpy and minimize compatibility test
-    """    
-    # Constrained and discrete optimization -----------------------------------    
+    """
+    # Constrained and discrete optimization -----------------------------------
     solution = minimize(**options)
-   
+
     check = np.array([21.0, -3, 0])
     assert len(check) == len(solution["best"]), "Solutions do not match form."
     for i in range(len(solution["best"])):
         assert solution["best"][i] == check[i], f"numpy test failed on {i}"
 
-        
 def test_scipy_test():
     """
     scipy.optimize.minimize compatibility test
-    """    
-    # Constrained and discrete optimization -----------------------------------    
+    """
+    # Constrained and discrete optimization -----------------------------------
     del options['bounds']
     del options['args']
     del options['fun']
-    
+
     def cb(x):
         pass
         #print(x)
-    
+
     solution = opt_min(_f, [0], bounds=_intvls, callback=cb,
                         method=leapfrog_method, options=options)
-   
+
     check = np.array([-3, 0])
     print(check, solution.x)
     assert len(check) == len(solution.x), "Solutions do not match form."
     for i in range(len(solution.x)):
         assert solution.x[i] == check[i], f"scipy test failed on {i}"
+    options['bounds'] = _intvls
+    options['args'] = ()
+    options['fun'] = _f
+
+
+def test_scipy_c():
+    """
+    scipy.optimize.minimize compatibility test
+    """
+    # Constrained and discrete optimization -----------------------------------
+    del options_c['bounds']
+    del options_c['args']
+    del options_c['fun']
+
+    def cb(x):
+        pass
+        #print(x)
+
+    solution = opt_min(_f, [0], bounds=_intvls, callback=cb,
+                        method=leapfrog_method, options=options_c)
+
+    check = np.array([-3, 0])
+    print(check, solution.x)
+    assert len(check) == len(solution.x), "Solutions do not match form."
+    for i in range(len(solution.x)):
+        assert solution.x[i] == check[i], f"scipy c test failed on {i}"
     options['bounds'] = _intvls
     options['args'] = ()
     options['fun'] = _f
